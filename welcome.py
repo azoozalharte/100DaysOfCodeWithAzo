a60993ab895888d1558d43ec7be301b5a0702b83
azoozalharte <zezo50115@protonmail.com>
2021-05-25 17:03:12 +0300

    Day-1: Print Method

--- welcome.py
+++ welcome.py
@@ -2,3 +2,11 @@ print('يا أهلا وسهلا')
 
 
 print('Introduction')
+
+
+print('=============DAY-1=============')
+print('Printing')
+print('Commenting')
+print('Debugging')
+print('String Manipulation')
+print('Variables')
